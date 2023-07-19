f56b6663540b8b3994602f2b2ed0193dc2ce26fb
Kishor <kishor.muruganandham@fssa.freshworks.com>
2023-07-19 11:47:26 +0530

    Added Day 08

--- src/main/java/day08/practice/employeeList.java
+++ src/main/java/day08/practice/employeeList.java
@@ -10,7 +10,7 @@ public class employeeList {
         HashMap<String, String> arr = new HashMap<>();
         Scanner sc = new Scanner(System.in);
         int x = 5;
-        while (x >= 0){
+        while (x > 0){
             String str = sc.nextLine();
             String[] strArr = str.split(",");
             String key = strArr[0],value = strArr[1].trim();
